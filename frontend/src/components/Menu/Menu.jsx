8a04f06afe4f84796716e0e6c8a769ed7e7ada20
Gaurav Raj <gaurav.raj@geeksforgeeks.org>
2024-10-05 11:52:21 +0530

    css upadted

--- frontend/src/components/Menu/Menu.jsx
+++ frontend/src/components/Menu/Menu.jsx
@@ -4,20 +4,20 @@ import './Menu.css'
 
 function Menu({dishes}) {
   return (
-    <div>
+    <div className='dishes-container'>
       {dishes.map((dish) => {
-  return (
-    <div className="dish-card" key={dish.id} onClick={() => {}}>
-      <img src={dish.image} alt={dish.name} className="dish-image" />
-      <div className="dish-body">
-        <div className="dish-price">Rs. {dish.price}</div>
-        <h3 className="dish-title">{dish.name}</h3>
-        <button className="add-to-cart-button" onClick={(e) => { 
-          e.stopPropagation();  }}> Add to Cart </button>
-      </div>
-    </div>
-  );
-})}
+                return (
+                  <div className="dish-card" key={dish.id} onClick={() => {}}>
+                    <img src={dish.image} alt={dish.name} className="dish-image" />
+                    <div className="dish-body">
+                      <div className="dish-price">Rs. {dish.price}</div>
+                      <h3 className="dish-title">{dish.name}</h3>
+                      <button className="add-to-cart-button" onClick={(e) => { 
+                        e.stopPropagation();  }}> Add to Cart </button>
+                    </div>
+                  </div>
+                );
+        })}
 
     </div>
   )
